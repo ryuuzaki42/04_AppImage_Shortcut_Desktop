94a1ffde503eb10a6a9f4db7f70c356ac0a9157b
João Batista <joao42lbatista@gmail.com>
2024-04-29 10:57:31 -0300

    Update Syncplay_run.sh, Teams_for_Linux_run.sh, and WinRAR_run.sh - add AppImage homepage

--- Syncplay/Syncplay_run.sh
+++ Syncplay/Syncplay_run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# AppImage homepage: https://github.com/ryuuzaki42/Syncplay_AppImage/releases
 set -x
 cd /media/sda2/os/gnu_linux/slackware15.0/AppImage/
 ./Syncplay-*.AppImage
--- WinRAR/WinRAR_run.sh
+++ WinRAR/WinRAR_run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# AppImage homepage: https://github.com/ryuuzaki42/WinRAR_AppImage/releases
 set -x
 cd /media/sda2/os/gnu_linux/slackware15.0/AppImage/
 ./WinRAR-*_JB.AppImage "$@"
