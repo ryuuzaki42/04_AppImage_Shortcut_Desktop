1681789832aab486c1606ffdd788146abde7b80c
João Batista <joao42lbatista@gmail.com>
2024-04-29 10:38:37 -0300

    Update Authy_run.sh, ChatALL_run.sh, and CodeBlocks_run.sh - add AppImage homepage

--- Authy/Authy_run.sh
+++ Authy/Authy_run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# AppImage homepage: https://github.com/ryuuzaki42/Authy_AppImage/releases
 set -x
 #cd $(grep "AppImage_Folder" ../README.md | cut -d '"' -f2) || exit
 cd /media/sda2/os/gnu_linux/slackware15.0/AppImage/
