8c20248eccf363f2e35ba87f2d306e3b95eef67d
João Batista <joao42lbatista@gmail.com>
2024-04-29 10:53:34 -0300

    Update xlaserpointer.desktop, xlaserpointer.png, and xlaserpointer_run.sh

---
+++ z_others_not_AppImage/xlaserpointer/xlaserpointer_run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Homepage Slackware Package: https://github.com/ryuuzaki42/xlaserpointer
+echo -e "\nCtrl + C to start next mode\n"
+set -x
+
+# default -s 7
+/usr/local/bin/xlaserpointer
+
+# " -s 10"
+/usr/local/bin/xlaserpointer -s 10
+
+# " -s 15"
+/usr/local/bin/xlaserpointer -s 15
+
+# " -s 15 -t 100"
+/usr/local/bin/xlaserpointer -s 15 -t 100
