9506724649042afedb5bb8a1227c59c18919dbe0
João Batista <joao42lbatista@gmail.com>
2024-04-29 09:53:46 -0300

    Update Authy.desktop and Authy_run.sh

--- Authy/Authy_run.sh
+++ Authy/Authy_run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -x
-cd $(grep "AppImage_Folder" ../README.md | cut -d '"' -f2) || exit
+#cd $(grep "AppImage_Folder" ../README.md | cut -d '"' -f2) || exit
+cd /media/sda2/os/gnu_linux/slackware15.0/AppImage/
 ./Authy_Desktop-*_JB.AppImage
