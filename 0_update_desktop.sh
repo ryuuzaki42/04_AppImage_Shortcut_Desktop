726d0901b45b80e7781d2f9206b46ab64fe08b39
João Batista <joao42lbatista@gmail.com>
2024-05-02 17:43:35 -0300

    Update 0_update_desktop.sh

--- 0_update_desktop.sh
+++ 0_update_desktop.sh
@@ -1,15 +1,38 @@
 #!/bin/bash
 # Update/Overwrite *.desktop information in ~/.local/share/applications/
-set -x
+#set -x
 
 #Git_Clone_Folder=$PWD
 
-# Default folde - Change in README.md
-#Git_Clone_Folder="/media/sda2/git_clone/AppImage_shortcut_desktop/"
-#AppImage_Folder="/media/sda2/os/gnu_linux/slackware15.0/AppImage/"
+## Change the path where is the *.AppImage in the */*_run.sh
+AppImage_Folder="/media/sda2/os/gnu_linux/slackware15.0/AppImage/"
 
-Git_Clone_Folder=$(grep "Git_Clone_Folder=" README.md | cut -d'"' -f2)
-AppImage_Folder=$(grep "AppImage_Folder=" README.md | cut -d'"' -f2)
+## Change the Icon and Exec in the */*.desktop
+#Git_Clone_Folder="/media/sda2/git_clone/AppImage_shortcut_desktop/"
+Git_Clone_Folder="$PWD/"
+
+## Comment Shortcut you don't want to install, like Authy line
+Shortcut_Install=("ChatALL"
+"CodeBlocks"
+"Discord"
+"Jubler"
+"KeePassXC"
+"MKVToolNix"
+"Maestral"
+"Opera"
+"Syncplay"
+"Teams_for_Linux"
+"WinRAR"
+"Zotero"
+"qBittorrent"
+"Audacity"
+#"Authy"
+#"PeaZip"
+#"Vivaldi"
+#"Xournalpp"
+)
+
+# --- Not need to change below this point ---
 
 echo -e "\nGit_Clone_Folder: $Git_Clone_Folder"
 echo -e "AppImage_Folder: $AppImage_Folder\n"
@@ -17,17 +40,23 @@ echo -e "AppImage_Folder: $AppImage_Folder\n"
 Git_Clone_Folder=$(echo "$Git_Clone_Folder" | sed 's/\//\\\//g') # Change / to \/ to sed
 AppImage_Folder=$(echo "$AppImage_Folder" | sed 's/\//\\\//g') # Change / to \/ to sed
 
-# Change Exec= and Icon=
-sed -i "s/Exec=.*AppImage_shortcut_desktop\//Exec=$Git_Clone_Folder/" */*.desktop
-sed -i "s/Icon=.*AppImage_shortcut_desktop\//Icon=$Git_Clone_Folder/" */*.desktop
+len=${#Shortcut_Install[@]}
+for (( i=0; i < len; i++)); do
+    Folder_Now=${Shortcut_Install[$i]}
+    echo -e "\nFolder $i: \"$Folder_Now\""
 
+    # Change Exec= and Icon=
+    sed -i "s/Exec=.*AppImage_shortcut_desktop\//Exec=$Git_Clone_Folder/" "$Folder_Now"/*.desktop
+    sed -i "s/Icon=.*AppImage_shortcut_desktop\//Icon=$Git_Clone_Folder/" "$Folder_Now"/*.desktop
 
-# Change path to the AppImage files - $AppImage_Folder
-sed -i "s/cd \/.*AppImage\//cd $AppImage_Folder/" */*_run.sh
+    # Change path to the AppImage files - $AppImage_Folder
+    sed -i "s/cd \/.*AppImage\//cd $AppImage_Folder/" "$Folder_Now"/*_run.sh
 
-# Change path to Git_Clone_Folder - Git_Clone_Folder - To mkvpropedit_run.sh for example
-sed -i "s/cd \/.*AppImage_shortcut_desktop\//cd $Git_Clone_Folder/" */*_run.sh
+    # Change path to Git_Clone_Folder - Git_Clone_Folder - To mkvpropedit_run.sh for example
+    sed -i "s/cd \/.*AppImage_shortcut_desktop\//cd $Git_Clone_Folder/" "$Folder_Now"/*_run.sh
 
-cp -v */*.desktop ~/.local/share/applications/
+    cp -v "$Folder_Now"/*.desktop ${HOME}.local/share/applications/
+done
 
+## Update KDE shortcuts information
 kbuildsycoca5
