b09243e1c38a547b64ef95f431015d8dc038df55
João Batista <joao42lbatista@gmail.com>
2024-05-02 17:46:40 -0300

    Update 0_update_desktop.sh

--- 0_update_desktop.sh
+++ 0_update_desktop.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
-# Update/Overwrite *.desktop information in ~/.local/share/applications/
-#set -x
+#
+# Autor= João Batista Ribeiro
+# Bugs, Agradecimentos, Críticas "construtivas"
+# Mande me um e-mail. Ficarei Grato!
+# e-mail: joao42lbatista@gmail.com
+#
+# Este programa é um software livre; você pode redistribui-lo e/ou
+# modifica-lo dentro dos termos da Licença Pública Geral GNU como
+# publicada pela Fundação do Software Livre (FSF); na versão 2 da
+# Licença, ou (na sua opinião) qualquer versão.
+#
+# Este programa é distribuído na esperança que possa ser útil,
+# mas SEM NENHUMA GARANTIA; sem uma garantia implícita de ADEQUAÇÃO a
+# qualquer MERCADO ou APLICAÇÃO EM PARTICULAR.
+#
+# Veja a Licença Pública Geral GNU para mais detalhes.
+# Você deve ter recebido uma cópia da Licença Pública Geral GNU
+# junto com este programa, se não, escreva para a Fundação do Software
+#
+# Livre(FSF) Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+#
+# Script: Update/Overwrite *.desktop information in ~/.local/share/applications/
+#
+# Last update: 02/05/2024
+#
 
-#Git_Clone_Folder=$PWD
+#set -x
 
 ## Change the path where is the *.AppImage in the */*_run.sh
 AppImage_Folder="/media/sda2/os/gnu_linux/slackware15.0/AppImage/"
