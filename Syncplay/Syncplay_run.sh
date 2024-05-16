a01a1e1561649efd7b2f20b82bd08450ee312e80
João Batista <joao42lbatista@gmail.com>
2024-05-16 08:23:59 -0300

    Update Syncplay_run.sh - _JB

--- Syncplay/Syncplay_run.sh
+++ Syncplay/Syncplay_run.sh
@@ -2,4 +2,4 @@
 # AppImage homepage: https://github.com/ryuuzaki42/Syncplay_AppImage/releases
 set -x
 cd /media/sda2/os/gnu_linux/slackware15.0/AppImage/
-./Syncplay-*.AppImage
+./Syncplay-*_JB.AppImage
