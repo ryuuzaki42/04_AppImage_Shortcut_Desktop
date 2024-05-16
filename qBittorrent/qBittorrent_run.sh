a55b20e48cdb998f8692c4dcce2eec7b1c3e5243
João Batista <joao42lbatista@gmail.com>
2024-05-16 08:24:52 -0300

    Update qBittorrent_run.sh

--- qBittorrent/qBittorrent_run.sh
+++ qBittorrent/qBittorrent_run.sh
@@ -2,6 +2,7 @@
 # AppImage homepage: https://www.qbittorrent.org/download#linux
 set -x
 cd /media/sda2/os/gnu_linux/slackware15.0/AppImage/
-./qbittorrent-*_lt20_x86_64.AppImage "$@"
+#./qbittorrent-*.AppImage "$@"
+./qbittorrent-*_lt20_*.AppImage "$@"
 
 # Tip: Add this script to Firefox instead the *.desktop to open Magnet Link
