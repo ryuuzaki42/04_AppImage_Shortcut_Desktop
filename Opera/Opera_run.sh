81341b028adf94fc73fe7194c110258721dea291
João Batista <joao42lbatista@gmail.com>
2024-07-13 10:14:26 -0300

    Opera_run.sh - Tag after verson not make error in zsync See https://github.com/ryuuzaki42/Opera_AppImage/releases/tag/112.0.5197.24

--- Opera/Opera_run.sh
+++ Opera/Opera_run.sh
@@ -2,7 +2,7 @@
 # AppImage homepage: https://github.com/ryuuzaki42/Opera_AppImage/releases
 set -x
 cd /media/sda2/os/gnu_linux/slackware15.0/AppImage/
-./Opera-*_JB.AppImage "$@"
+./Opera-*_JB*.AppImage "$@"
 
 ## Options
     # --incognito
