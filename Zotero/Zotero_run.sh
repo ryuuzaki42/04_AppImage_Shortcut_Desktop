8d1b7fa50dc524bbef4e068b7c41f4220b3f15b2
João Batista <joao42lbatista@gmail.com>
2024-08-27 14:25:43 -0300

    Update Zotero_run.sh - Tag (1_JB) after the version, to not give error in zsync https://github.com/ryuuzaki42/Zotero_AppImage

--- Zotero/Zotero_run.sh
+++ Zotero/Zotero_run.sh
@@ -2,4 +2,4 @@
 # AppImage homepage: https://github.com/ryuuzaki42/Zotero_AppImage/releases
 set -x
 cd /media/sda2/os/gnu_linux/slackware15.0/AppImage/Zotero/
-./Zotero-*_JB.AppImage "$@"
+./Zotero-*_JB*.AppImage "$@"
