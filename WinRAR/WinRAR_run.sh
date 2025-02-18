a784fcd94809ecafe45e6a27ad1b69393d107054
João Batista <joao42lbatista@gmail.com>
2025-02-18 15:58:41 -0300

    Update WinRAR_run.sh

--- WinRAR/WinRAR_run.sh
+++ WinRAR/WinRAR_run.sh
@@ -2,4 +2,4 @@
 # AppImage homepage: https://github.com/ryuuzaki42/WinRAR_AppImage/releases
 set -x
 cd /media/sda2/os/gnu_linux/slackware15.0/AppImage/
-./WinRAR-*_JB.AppImage "$@"
+./WinRAR-*_JB*.AppImage "$@"
