196da2838b0897302f1c61dc3f73d26604fb56c8
João Batista <joao42lbatista@gmail.com>
2025-02-18 15:58:32 -0300

    Update Xournalpp_run.sh

--- Xournalpp/Xournalpp_run.sh
+++ Xournalpp/Xournalpp_run.sh
@@ -2,4 +2,6 @@
 # AppImage homepage: https://github.com/xournalpp/xournalpp/releases
 set -x
 cd /media/sda2/os/gnu_linux/slackware15.0/AppImage/
+
+#https://github.com/ryuuzaki42/AppImage_Full_ArchImage/releases
 ./xournalpp-*.AppImage "$@"
