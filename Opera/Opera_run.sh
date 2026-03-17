826b4d73e14614e3b2d20439da7ac575ca6db515
João Batista <joao42lbatista@gmail.com>
2026-03-17 18:15:26 -0300

    Maestral_run.sh, Opera_run.sh, Syncplay_run.sh, WinRAR_run.sh, Zotero_run.sh - "*_JB-x86_64.AppImage"

--- Opera/Opera_run.sh
+++ Opera/Opera_run.sh
@@ -2,7 +2,7 @@
 # AppImage: https://github.com/ryuuzaki42/Opera_AppImage/releases
 set -x
 cd /media/sda2/os/gnu_linux/slackware15.0/AppImage/
-./Opera-*_JB*.AppImage "$@"
+./Opera-*_JB-x86_64.AppImage "$@"
 
 ## Options
     # --incognito
--- Syncplay/Syncplay_run.sh
+++ Syncplay/Syncplay_run.sh
@@ -2,4 +2,4 @@
 # AppImage: https://github.com/ryuuzaki42/Syncplay_AppImage/releases
 set -x
 cd /media/sda2/os/gnu_linux/slackware15.0/AppImage/
-./Syncplay-*_JB.AppImage
+./Syncplay-*_JB-x86_64.AppImage
