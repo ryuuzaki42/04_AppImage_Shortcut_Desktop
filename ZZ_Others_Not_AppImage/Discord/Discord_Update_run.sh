841d62a2e34b2d069e31dcd80db5c6140d6b1fa4
João Batista <joao42lbatista@gmail.com>
2026-05-04 21:13:02 -0300

    Update Discord.desktop and Discord_Update_run.sh

--- ZZ_Others_Not_AppImage/Discord/Discord_Update_run.sh
+++ ZZ_Others_Not_AppImage/Discord/Discord_Update_run.sh
@@ -3,17 +3,17 @@
 # https://discord.com/download
 # Download a new version and extract in the folder
 #
-# Last update: 09/08/2024
+# Last update: 04/05/2026
 #
 set -x
 
 place_install="/media/sda2/0_installed/Discord/"
-folder_RAM="/media/sda2/home/j/Downloads/0_tmp_RAM_disk/"
+folder_RAM="/media/sda2/home/j/Downloads/0_tmp_folder_RAM/"
 
 cd "$folder_RAM"
 wget "https://discord.com/api/download?platform=linux&format=tar.gz" -O discord.tar.gz
 
-tar -xf discord.tar.gz -C "$place_install/"
+tar -xf discord.tar.gz -C "$place_install"
 rm discord.tar.gz
 
 echo -e "\nHit enter to terminate"
