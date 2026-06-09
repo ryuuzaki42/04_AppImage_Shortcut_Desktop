41737927b24a4864c42a76fda0f6274dba5119e5
João Batista <joao42lbatista@gmail.com>
2026-06-09 11:11:41 -0300

    Update MATLAB_run.sh

--- ZZ_Others_Not_AppImage/MATLAB/MATLAB_run.sh
+++ ZZ_Others_Not_AppImage/MATLAB/MATLAB_run.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+# Homepage: https://www.mathworks.com/products/matlab.html
+
 set -x
 
 ## Change to folder were MATLAB is installed
-cd /media/sda2/0installed/Matlab/R2021a/bin/
+matlab_path="/media/sda2/0installed/Matlab/R2021a/bin/"
+
+cd "$matlab_path"
 
 env MESA_LOADER_DRIVER_OVERRIDE=i965 ./matlab -desktop
