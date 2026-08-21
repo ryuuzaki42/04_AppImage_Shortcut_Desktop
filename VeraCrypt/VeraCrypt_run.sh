a76a140915f6f402a93a0785335477e92f5e1f13
João Batista <joao42lbatista@gmail.com>
2026-08-21 16:00:27 -0300

    Update VeraCrypt_run.sh

--- VeraCrypt/VeraCrypt_run.sh
+++ VeraCrypt/VeraCrypt_run.sh
@@ -3,11 +3,11 @@
 # AppImage: https://github.com/veracrypt/VeraCrypt/releases
 set -x
 
-cd /media/sda2/home/j/Dropbox/os/gnu_linux/slackware15.0/AppImage/
-chmod +x VeraCrypt-*.AppImage
-
 root_command="kdesu"    # Change kdesu to the root command
 file_explorer="dolphin" # Change dolphin for the file explorer
+
+cd /media/sda2/home/j/Dropbox/os/gnu_linux/slackware15.0/AppImage/
+chmod +x VeraCrypt-*.AppImage
 
 echo -e "\nNeed to run as root!\n"
 $root_command ./VeraCrypt-*.AppImage &
